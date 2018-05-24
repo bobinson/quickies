cf2510c954e5dd646c97b2bace55f1e33d980e7e
Bobinson K B <bobinson@gmail.com>
2018-05-25 01:53:37 +0530

    Update essential-dev-tools.sh

--- essential-dev-tools.sh
+++ essential-dev-tools.sh
@@ -1,1 +1,1 @@
-apt install build-essential clang libssl-dev zlib1g-dev libncurses5-dev libreadline-dev unzip zip
+apt install build-essential clang libssl-dev zlib1g-dev libncurses5-dev libreadline-dev unzip zip libsqlite3-dev
