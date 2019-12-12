2ff3a44d2d07d56ab34072c67fc861717fe61297
Bobinson K B <bobinson@gmail.com>
2019-12-12 11:53:02 +0530

    Create typical-ubuntu.sh

---
+++ typical-ubuntu.sh
@@ -0,0 +1,12 @@
+apt --list-installed
+apt update
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B
+apt-get install apt-fast
+apt update
+apt-get install apt-fast
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get -y install apt-fast
+apt-fast -y dist-upgrade
+sudo add-apt-repository ppa:synapse-core/ppa
+apt-fast -y install  macbuntu-os-icons-v1804  macbuntu-os-ithemes-v1804   gnome-tweak-tool  libreoffice-style-sifr  synapse konsole vim
