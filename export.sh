2a10fe6db0c27ae2db75fad3ce49033169704b19
bobinson <bobinson@gmail.com>
2020-08-04 02:32:39 +0530

    export.sh

---
+++ export.sh
@@ -0,0 +1,4 @@
+ export BOOST_ROOT=$HOME/src/boost/boost_1_56_0
+ export BOOST_ROOT=$HOME/src/boost/boost_1_56_0
+ export BOOST_ROOT=$HOME/src/boost/boost_1_56_0
+ export BOOST_INCLUDEDIR=$BOOST_ROOT
